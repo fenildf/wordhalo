498f774906b6818da454e3f0492a4811f93fc6a7
Joshua Zhang <woncomp@gmail.com>
2016-06-24 13:57:01 +0000

    Fixed: change redis url environment variable name.

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,6 +1,5 @@
 # http://jerodsanto.net/2013/01/configuring-redis-store-redis-rails-on-heroku/
 
-redis_url = ENV["REDISCLOUD_URL"] || "redis://127.0.0.1:6379/wordhalo"
-puts redis_url
+redis_url = ENV["REDIS_URL"] || "redis://127.0.0.1:6379/wordhalo"
 Rails.application.config.cache_store = :redis_store, redis_url
 Rails.application.config.session_store :redis_store, redis_server: redis_url
