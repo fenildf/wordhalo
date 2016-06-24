95b240ebe735b23fb75163eb3ed9a5b00871e3cc
Joshua Zhang <woncomp@gmail.com>
2016-06-24 14:12:58 +0000

    Add expiration time for redis cache.

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,5 +1,5 @@
 # http://jerodsanto.net/2013/01/configuring-redis-store-redis-rails-on-heroku/
 
 redis_url = ENV["REDIS_URL"] || "redis://127.0.0.1:6379/wordhalo"
-Rails.application.config.cache_store = :redis_store, redis_url
+Rails.application.config.cache_store = :redis_store, redis_url, { expires_in: 90.minutes }
 Rails.application.config.session_store :redis_store, redis_server: redis_url
