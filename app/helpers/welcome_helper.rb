d92c8c1276004fc454289687b24243123fc7e94d
Joshua Zhang <woncomp@gmail.com>
2016-06-27 04:10:27 +0000

    Display the amount of cards scheduled prior to tomorrow on the home page.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,9 @@
 module WelcomeHelper
+    def today_cards_count
+        @user.cards.where(["schedule < ?", DateTime.tomorrow]).count
+    end
+    
+    def total_cards_count
+        @user.cards.count
+    end
 end
