33ae75919f9b7b472be93940d1d7099410b12df9
Joshua Zhang <woncomp@gmail.com>
2016-07-15 03:54:41 +0000

    Fixed: Todays remaining cards count calculated wrong.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,6 +1,6 @@
 module WelcomeHelper
     def today_cards_count
-        @user.cards.where(["DATE(created_at) < ?", Date.tomorrow]).count
+        @user.cards.where(["DATE(schedule) < ?", Date.tomorrow]).count
     end
     
     def total_cards_count
